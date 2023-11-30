f62419d6ea80075246e6feeb2e34f4aca07d44e8
Jingning <seamissu@gmail.com>
2023-11-30 22:54:21 +1100

    finish Web design page desktop version

--- src/components/WebMain/WebMain.js
+++ src/components/WebMain/WebMain.js
@@ -3,6 +3,7 @@ import styles from './WebMain.module.scss';
 
 import DesignHero from '../DesignHero';
 import Card from '../Card';
+import SuperLink from '../SuperLink';
 
 function WebMain() {
   return (
@@ -15,6 +16,23 @@ function WebMain() {
         <Card src={'/web-design/desktop/image-builder.jpg'} />
         <Card src={'/web-design/desktop/image-blogr.jpg'} />
         <Card src={'/web-design/desktop/image-camp.jpg'} />
+      </section>
+
+      <section className={styles.designwrapper}>
+        <SuperLink
+          className={styles.onlyondesktop}
+          type={'App'}
+          height={308}
+          bgImageUrl={'/home/desktop/image-app-design.jpg'}
+          linkURL={'/'}
+        />
+        <SuperLink
+          className={styles.onlyondesktop}
+          type={'Graphic'}
+          height={308}
+          bgImageUrl={'/home/desktop/image-graphic-design.jpg'}
+          linkURL={'/'}
+        />
       </section>
     </main>
   );
